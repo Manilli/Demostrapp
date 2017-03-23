59c5a102bbdc9c157b9f5cb05acf4d3ad57627a5
Manilli <manillikun@gmail.com>
2017-03-23 15:34:25 -0500

    nuevoteorema

---
+++ app/src/main/java/com/example/johnjairo/demostrapp/NuevoTeorema.java
@@ -0,0 +1,16 @@
+package com.example.johnjairo.demostrapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Created by rocka on 23/03/2017.
+ */
+
+public class NuevoTeorema extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nuevoteorema);
+    }
+}
