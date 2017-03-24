ef006e9cf72dc85f49e0f5458a443bf6ba5abe46
Manilli <manillikun@gmail.com>
2017-03-24 01:04:58 -0500

    commit2

--- app/src/main/java/com/example/johnjairo/demostrapp/NuevoTeorema.java
+++ app/src/main/java/com/example/johnjairo/demostrapp/NuevoTeorema.java
@@ -2,7 +2,9 @@ package com.example.johnjairo.demostrapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
 /**
  * Created by rocka on 23/03/2017.
  */
@@ -12,5 +14,19 @@ public class NuevoTeorema extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nuevoteorema);
+
+        // Esto es para el spinner
+        Spinner spinner = (Spinner) findViewById(R.id.spinner_inferencia);
+        ArrayAdapter<CharSequence> inferenciaAdapter = ArrayAdapter.createFromResource(this,
+                R.array.premisa_conclusion_array, android.R.layout.simple_spinner_dropdown_item);
+        inferenciaAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(inferenciaAdapter);
+
+        Spinner spinner2 = (Spinner) findViewById(R.id.spiner_justificacion);
+        ArrayAdapter<CharSequence> justificacion_Adapter = ArrayAdapter.createFromResource(this,
+                R.array.justificacion_array, android.R.layout.simple_spinner_dropdown_item);
+        justificacion_Adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(justificacion_Adapter);
+
     }
 }
